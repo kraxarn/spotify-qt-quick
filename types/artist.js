1969a919636d9e860b0692589f5512051ef6996c
kraxarn <me@kraxarn.com>
2021-04-17 10:08:19 +0200

    Initial type annotations

---
+++ types/artist.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef Artist
+ * @property {string} image
+ * @property {string} name
+ * @property {Array<string>} genres
+ * @property {number} followers
+ * @property {string} id
+ */
---
+++ types/track.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef Track
+ * @property {string} id
+ * @property {string} artist
+ * @property {string} name
+ * @property {string} artist_id
+ * @property {string} album_id
+ */
