d26f45720453b0d6513535f639f7fa47d59c861d
kraxarn <me@kraxarn.com>
2021-04-26 20:36:31 +0200

    Standalone main

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include "appversion.hpp"
+#include "qmlmanager.hpp"
+#include "lib/qtquickpaths.hpp"
+
+#include <QApplication>
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+auto main(int argc, char **argv) -> int
+{
+	// Set name for settings etc.
+	QCoreApplication::setOrganizationName("kraxarn");
+	QCoreApplication::setApplicationName("spotify-qt");
+	QCoreApplication::setApplicationVersion(APP_VERSION);
+
+	// High-DPI support
+	// These flags are deprecated and always enabled in Qt 6 and newer
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+
+	// Create Qt application
+	QApplication app(argc, argv);
+
+	// Settings
+	QtQuickPaths paths(nullptr);
+	lib::settings settings(paths);
+
+	// Create QML engine
+	QmlManager qml(settings);
+
+	// Command line parameters
+	QCommandLineParser parser;
+	parser.addVersionOption();
+	parser.addHelpOption();
+	parser.addOptions({
+		{"dev", "Enable developer mode for troubleshooting issues."},
+		{"reset-credentials", "Allows providing new Spotify credentials."}
+	});
+	parser.process(app);
+
+	if (parser.isSet("dev"))
+	{
+		lib::developer_mode::enabled = true;
+	}
+
+	// First setup
+	if (settings.account.refresh_token.empty()
+		|| parser.isSet("reset-credentials"))
+	{
+		if (qml.setup())
+		{
+			return 0;
+		}
+	}
+
+	// Create main view
+	qml.main();
+
+	// Run
+	return QApplication::exec();
+}
